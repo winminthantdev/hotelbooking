a601bd4cef259866c69470ae2f4a42e05f93df36
soemyatthu <soemyat4525@gmail.com>
2025-09-26 00:11:20 +0630

    Added welcome component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 
-
+import Welcome from "../components/Welcome";
 
 const HomePage = ()=>{
   return(
     <>
       <Header />
       <Navbar />
+      <Welcome />
       
     </>
   )

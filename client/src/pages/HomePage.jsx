99bca4de5654f6d39b04310f429e20640ddd5361
winminthant <winminthant.gwin@gmail.com>
2025-09-26 14:46:52 +0630

    fix:home

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -17,12 +17,3 @@ const HomePage = ()=>{
 
 export default HomePage;
 
-<div className="col-md-4 col-sm-12 mb-3">
-            <div className="card p-3 p-lg-5 p-md-4 facilitycards">
-              <h5 className="text-warning p-lg-3 p-md-4">5</h5>
-              <p className="fw-semibold text-muted mb-3">On-site Parking</p>
-              <p className="text-center text-secondary mb-3">
-                Safe and secure parking available for all guests.
-              </p>
-            </div>
-          </div>
